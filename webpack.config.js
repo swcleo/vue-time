df3c01124d5e29dce2c8866c1f332391e44da6af
Leo <cswleo@gmail.com>
2018-09-24 23:48:40 +0800

    build(webpack): ie 11

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'main.js',
+    library: 'VueTime',
+    libraryTarget: 'umd'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      moment: 'moment'
+    }),
+    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+  ]
+}
